f8cd53c52c03810cbebd9d809db1ae10ad4ef655
Justin Donn <jdonn@JDMBP.hsd1.ca.comcast.net>
2019-01-27 05:22:02 -0800

    typo

--- game/script.js
+++ game/script.js
@@ -82,12 +82,7 @@ ws.onmessage = function(event) {
 }
 
 function updateVariables() {
-	if (holdCards[0] > -1) {
-		updateCards("first-card", holeCards[0]); // hole card 1 @ interface
-		updateCards("second-card", holeCards[1]); // hole card 2 @ interface
-		updateCards("first-p" + ((myIndex + 1).toString()), holeCards[0]); // hole card 1 @ playerspace
-		updateCards("second-p" + ((myIndex + 1).toString()), holeCards[1]); // hole card 2 @ playerspace
-	}
+	updateHoldCards();
 	updateCommunityCards(); // flop, turn, river
 	updateCurrentTurn(); // show user input if it's my turn, change current player's background to blue
 	updateDealerStacksAndNames();
@@ -98,6 +93,15 @@ function updateVariables() {
 function updateCards(cardID, fileName) {
 	var x = document.getElementById(cardID);
 	x.setAttribute("src", "../images/cards/" + fileName + ".png");
+}
+
+function updateHoldCards() {
+	if (holeCards[0] > -1) {
+		updateCards("first-card", holeCards[0]); // hole card 1 @ interface
+		updateCards("second-card", holeCards[1]); // hole card 2 @ interface
+		updateCards("first-p" + ((myIndex + 1).toString()), holeCards[0]); // hole card 1 @ playerspace
+		updateCards("second-p" + ((myIndex + 1).toString()), holeCards[1]); // hole card 2 @ playerspace
+	}
 }
 
 function updateCommunityCards() {
