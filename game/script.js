667275aa769cfcc2e2c436d77ae3a660c46d0f65
Justin Donn <jdonn@JDMBP.hsd1.ca.comcast.net>
2019-01-27 06:39:30 -0800

    small fixes

--- game/script.js
+++ game/script.js
@@ -11,7 +11,7 @@
 var ws = new WebSocket("ws://poker.mkassaian.com:8080");
 var myName = localStorage.getItem("username");
 var myBuyIn = localStorage.getItem("buyin");
-ws.onopen=(e)=>ws.send(myBuyIn + " " + name);
+ws.onopen=(e)=>ws.send(myBuyIn + " " + myName);
 setInterval(()=>ws.send("1"), 2000);
 var myIndex = -1; // this is set when client receives data from server
 var inPlayers = [];
