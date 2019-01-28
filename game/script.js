b663915466cbf17884d95516ce5154b1e4e73517
Justin Donn <jdonn@JDMBP.local>
2019-01-28 02:13:01 -0800

    new approach to animations

--- game/script.js
+++ game/script.js
@@ -14,6 +14,7 @@
 // 		 allow people to pick seats
 // 		 display winner of the hand
 //		 add sound to signal your turn/moving chips around/when you check
+// 		 properly discard of kicked players
 
 var ws = new WebSocket("ws://poker.mkassaian.com:8080");
 var myName = localStorage.getItem("username").trim().substring(0, 15); // limit size of name to be 15 chars
@@ -29,13 +30,7 @@ function error(e) {
 var myIndex = -1; // this is set when client receives data from server later
 var inPlayers = [];
 var newRound = true;
-//var newBettingRound = true;
-//var prevTurn;
-//var prevAction;
 var veryFirst = true;
-//var alreadyAnimated;
-//var prevActionIndex;
-//var prevActionMessage;
 var prevHand = -1;
 
 
@@ -53,8 +48,7 @@ var stacks;
 var playerNames;
 var pot;
 var folded;
-var actionMessage;
-var actionIndex;
+var actions;
 var handNumber;
 
 var dataArray;
@@ -72,36 +66,20 @@ ws.onmessage = function(event) {
 	playerNames = dataDict["names"];
 	pot = dataDict["pot"];
 	folded = dataDict["folded"];
-	actionMessage = dataDict["action_message"];
-	actionIndex = dataDict["action_index"];
 	handNumber = dataDict["hand_number"];
-	window.alert(actionMessage + " + " + handNumber);
-
-
-	if (handNumber != prevHand) {
-		// if the dealer position has moved, it is a new round
-		newRound = true;
-		prevHand = handNumber;
-
-		// if there are a differing number of -1's between board cards, it is a new betting round
-		/*if (countInArray(communityCards, -1) != countInArray(dataDict["board_cards"], -1)) {
-			newBettingRound = true;
-		} else {
-			newBettingRound = false;
-		} */
-	} else {
-		newRound = false;
-	}
-
-	// this has to be after the new round check!!
 	dealer = dataDict["dealer"];
-
-	// this has to be after the new betting round check!!
 	communityCards = dataDict["board_cards"];
+	actions = dataDict["actions"];
 
 	// set myIndex, should only have to do once
 	myIndex = playerNames[myName];
 
+	if (handNumber != prevHand) {
+		newRound = true;
+		prevHand = handNumber;
+	} else {
+		newRound = false;
+	}
 	// reset inPlayers and hide fold message at the start of each new round 
 	if (newRound) {
 		if (numPlayers >= 2) {
@@ -114,11 +92,6 @@ ws.onmessage = function(event) {
 		}
 		
 		resetGame();
-		//document.getElementById("fold-message").style.visibility = "hidden";
-		//prevActionIndex = -1;
-		//prevActionMessage = "";
-		//actionIndex = -1;
-		//actionMessage = "";
 	} 
 
 	// update playerspaces (make visible if the player exists)
@@ -126,57 +99,14 @@ ws.onmessage = function(event) {
 		document.getElementById("p" + i.toString()).getElementsByClassName("toggle-visibility")[0].style.visibility = "visible";
 	}
 
-	/* if (typeof prevTurn == "undefined") {
-		prevTurn = (currPlayerTurn -1) % numPlayers;
-		prevAction = "";
-	} */
-
-	/* if (typeof prevTurn != "undefined" && prevTurn != (currPlayerTurn -1) % numPlayers) {
-		animateAction(prevTurn, prevAction);
-	} */
-	/*if (newBettingRound) {
-
-	} */
-	if ((actionIndex !== -1) && (actionMessage !== "")) {
+	/* if ((actionIndex !== -1) && (actionMessage !== "")) {
 		window.alert("inside that if statement");
 		animateAction(actionIndex, actionMessage);
-		//prevActionIndex = actionIndex;
-		//prevActionMessage = actionMessage;
+	} */
+	// unnecesssary check, but why not
+	if (actions.length != 0) {
+		animations(actions);
 	}
-	/*
-	var nextPersonsTurn = (prevTurn != (currPlayerTurn - 1 + numPlayers) % numPlayers);
-	// prevTurn = (currPlayerTurn -1) % numPlayers;
-
-	// find prevAction
-	//if (!newBettingRound) {
-		if (folded[prevTurn] == 1) {
-			prevAction = "Fold";
-		} else if (bets[prevTurn] == 0) {
-			prevAction = "Check";
-		} else {
-			var tempMax = 0;
-			for (i = 0; i < numPlayers; i++) {
-				if (prevTurn != i && bets[i] > tempMax) {
-					tempMax = bets[i];
-				}
-			}
-			if (bets[prevTurn] > tempMax) {
-				prevAction = "Raise to " + bets[prevTurn].toString();
-			} else {
-				prevAction = "Call " + tempMax.toString();
-			}
-		}
-	} else {
-		prevAction = false;
-	} 
-	if (!veryFirst && (nextPersonsTurn) && alreadyAnimated != prevTurn) {
-		animateAction(prevTurn, prevAction);
-		nextPersonsTurn = false;
-		alreadyAnimated = prevTurn;		
-	}
-	prevTurn = (currPlayerTurn - 1 + numPlayers) % numPlayers;
-
-	*/
 	updateVariables();
 	if (riverHoleCards.length > 1 && inPlayers.length != 0) {
 		showHoleCardsAtEnd();
@@ -192,16 +122,6 @@ function updateVariables() {
 	updateDealerStacksAndNames();
 	updateBetsAndFolds();
 	updatePot();
-}
-
-function countInArray(array, element) {
-	var count = 0;
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] == element) {
-            count++;
-        }
-    }
-    return count;
 }
 
 function updateCards(cardID, fileName, playingCard = true) {
@@ -298,28 +218,6 @@ function updateCurrentTurn() {
 
 		}
 	}
-	/* if (actualCurrPlayer == myIndex) {
-		// display current bet on Call button
-		document.getElementById("call").innerHTML = "Call " + Math.max(bets).toString();
-		showUserInput();
-	} else {
-		hideUserInput();
-	} */
-	// FIX THIS
-	// i think this works
-}
-
-/* function hideUserInput () {
-	var toHide = document.getElementById("user-input").getElementsByClassName("toggle-visibility")[0];
-	toHide.style.visibility = 'hidden';
-	no idea if this works 
-} */
-
-/* function showUserInput () {
-	var toShow = document.getElementById("user-input").getElementsByClassName("toggle-visibility")[0];
-	toShow.style.visibility = 'visible';
-	no idea if this works
-} */
 
 function updateDealerStacksAndNames() {
 	for (i = 0; i < numPlayers; i++) {
@@ -340,14 +238,7 @@ function updateBetsAndFolds() {
 		if (bets[i] == -1) { // fold
 			inPlayers.splice(i - removed, 1);
 			removed++;
-			document.getElementById("fold-message").style.visibility = "visible";
-			/* animateAction(i, "Fold"); */
 		} else { // check or call or raise
-			/*if (bets[i] == 0) {
-				animateAction(i, "Check");
-			} else {
-				animateAction(i, "Raise to " + bets[i].toString());
-			} */
 			document.getElementById("bet-size-p" + i.toString()).innerHTML = bets[i].toString();
 		}
 		if (folded[i] == 1) {
@@ -380,6 +271,35 @@ function animateAction(playerID, message) {
 	// pretty sure this works actually
 }
 
+function animations(actionList) {
+	for (i = 0; i < actionList.length; i++) {
+		var sender = parseMessage(actionList[i])[0];
+		var actualMessage = parseMessage(actionList[i])[1];
+		beginAnimation(sender, actualMessage);
+	}
+	for (i = 0; i < actionList.length; i++) {
+		var sender = parseMessage(actionList[i])[0];
+		endAnimation(sender);
+	}
+}
+
+function parseMessage(message) {
+	var returnList = [];
+	returnList[0] = message.substring(0, 1);
+	returnList[1] = message.substring(1);
+	return returnList;
+}
+
+function beginAnimation(playerID, message) {
+	var player = document.getElementById("action-text-p" + playerID.toString());
+	player.querySelector(".action-text p").innerHTML = message;
+	player.classList.add("action-text-transition");
+}
+
+function endAnimation(playerID) {
+	var player = document.getElementById("action-text-p" + playerID.toString());
+	player.classList.remove("action-text-transition");
+}
 
 function changeRaise(scalar) {
 	var raiseSize = scalar * pot;
@@ -392,29 +312,29 @@ function updatePot() {
 }
 
 function send(arg) {
-	// data in form of: "0,*integer corresponding to bet/check*,*action message for animations*,*myIndex*)
+	// data in form of: "0,*integer corresponding to bet/check*,*myIndex + action message for animations*)
 	var data;
 	if (arg == "raise") {
 		var raiseAmount = document.getElementById("raise-amount").value;
-		data = "0," + raiseAmount.toString()  + ",Raise to " + raiseAmount.toString() + "," + myIndex.toString();
+		data = "0," + raiseAmount.toString()  + "," +  myIndex.toString() + "Raise to " + raiseAmount.toString();
 	}
 	if (arg == "check/call") {
 		var maxBet = Math.max.apply(null, bets);
 		if (maxBet > stacks[myIndex] + bets[myIndex]) {
-			data = "0," + (stacks[myIndex] + bets[myIndex]).toString() + ",All in for " + (stacks[myIndex] + bets[myIndex]).toString() + "," + myIndex.toString();;
+			data = "0," + (stacks[myIndex] + bets[myIndex]).toString() + "," + myIndex.toString() + "All in for " + (stacks[myIndex] + bets[myIndex]).toString();
 		} else {
-			data = "0," + maxBet.toString() + ",Call " + maxBet.toString() + "," + myIndex.toString();
+			data = "0," + maxBet.toString() + "," + myIndex.toString() + "Call " + maxBet.toString();
 		}
 	}
 	if (arg == "allin") {
 		var allInAmount = stacks[myIndex] + bets[myIndex];
-		data = "0," + allInAmount.toString() + ",All in for " + allInAmount.toString() + "," + myIndex.toString();
+		data = "0," + allInAmount.toString() + "," + myIndex.toString() + "All in for " + allInAmount.toString();
 	} else if (arg == "leave") {
 		data = "";
 	} else if (arg == -1) {
-		data = "0,-1," + "Fold," + myIndex.toString();
+		data = "0,-1," + myIndex.toString() + "Fold";
 	} else if (typeof arg === "number") {
-		data = "0," + arg.toString() + ",Bet " + arg.toString() + "," + myIndex.toString();
+		data = "0," + arg.toString() + "," + myIndex.toString() + "Bet " + arg.toString();
 	}
 	document.getElementById("raise-amount").value = ""; // clear raise input field
 	ws.send(data);
