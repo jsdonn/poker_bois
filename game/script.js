75f1f98c91fb39400acec66df8370074eae1ec68
Justin Donn <jdonn@JDMBP.hsd1.ca.comcast.net>
2019-02-01 02:37:33 -0800

    hopefully animations are longer now

--- game/script.js
+++ game/script.js
@@ -376,16 +376,16 @@ function parseMessage(message) {
 function beginAnimation(playerIndex, message) {
 	var player = document.getElementById("action-text-p" + playerIndex);
 	player.querySelector(".action-text p").innerHTML = message;
+	player.style.opacity = "1";
 	player.style.transition = "none";
 	player.style.visibility = "visible";
-	player.style.opacity = "1";
 }
 
 function endAnimation(playerIndex) {
 	var player = document.getElementById("action-text-p" + playerIndex);
+	player.style.opacity = "0";
 	player.style.transition = "visibility 0s linear .5s, opacity .5s linear";
 	player.style.visibility = "hidden";
-	player.style.opacity = "0";
 }
 
 function changeRaise(scalar) {
